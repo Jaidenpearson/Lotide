8a00d3d09616b7fe9ab3e8ef77ecbd1ad17ccc85
Jaidenpearson <[jaidensrpearson@gmail.com]>
2024-09-30 12:44:40 -0700

    added emojis for better visibility

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,15 @@
 const assertArraysEqual = function(arr1, arr2) {
   if(arr1.length !== arr2.length) {
-    console.log("Arrays are different lengths")
+    console.log("🛑🛑🛑Arrays are different lengths")
     return
   }
   for(let i = 0; i < arr1.length; i++) {
     if(arr1[i] !== arr2[i]) {
-      console.log(`Index: ${[i]} is not equal between arrays`)
+      console.log(`🛑🛑🛑Index: ${[i]} is not equal between arrays`)
       return
     }
   }
-  console.log("Arrays are identical")
+  console.log("✔✔✔Arrays are identical")
 }
 
 // TEST CODE
