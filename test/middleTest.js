88c2f627812d1fcfda4ceede4499b6d758dc542f
Jaidenpearson <[jaidensrpearson@gmail.com]>
2024-10-18 16:05:29 -0700

    testing tail and middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,18 @@
-const assertArraysEqual = require("../assertArraysEqual")
+const assert = require('chai').assert
 const middle = require("../middle")
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+describe("#middle", () => {
+  it("Returns an empty array when given a single index", () => {
+    assert.deepEqual(middle([1]), [])
+  })
+  it("Returns two digits when given an array divisible by 2", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+  })
+})
+
+// assertArraysEqual(middle([1]), []); // => []
+// assertArraysEqual(middle([1, 2]), []); // => []
+// assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,12 @@
-const assertEqual = require("../assertEqual")
+const assert = require("chai").assert
 const tail = require("../tail")
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Lab");
+describe("#tail", () => {
+  it("returns [2, 4, 5] from [1, 2, 4, 5]", () => {
+    assert.deepEqual(tail([1, 2, 4, 5]), [2, 4, 5])
+  })
+  it("returns ['lighthouse'] from ['labs', 'ligthhouse']", () => {
+    assert.deepEqual(tail(['labs', 'lighthouse']), ['lighthouse'])
+  })
+})
+
