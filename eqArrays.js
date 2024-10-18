b3b120568dc9736a6f596e97dff39283a8fbeae3
Jaidenpearson <[jaidensrpearson@gmail.com]>
2024-10-18 13:13:09 -0700

    refactored eqArrays and created test file

--- eqArrays.js
+++ eqArrays.js
@@ -11,17 +11,4 @@ const eqArrays = function(arr1, arr2) {
   return true
 }
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected} `);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays
