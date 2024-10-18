265a2ef06a703546b5267157b1529b45762b9e44
Jaidenpearson <[jaidensrpearson@gmail.com]>
2024-10-18 13:15:11 -0700

    removed console.log

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,8 @@
-const eqArrays = function(arr1, arr2) {
+const eqArrays = (arr1, arr2) => {
   if(arr1.length !== arr2.length) {
     return false
   }
   for(let i = 0; i < arr1.length; i++) {
-    console.log(arr1[i], arr2[i])
     if(arr1[i] !== arr2[i]) {
       return false
     }
