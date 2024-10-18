5d44a712f732a8b58cb6d8a62c26723095332d3a
Jaidenpearson <[jaidensrpearson@gmail.com]>
2024-10-18 13:05:52 -0700

    refactored tail

--- tail.js
+++ tail.js
@@ -1,25 +1,4 @@
-const tail = function(arr) {
-  let newarr = arr.slice(1);
-  return newarr;
-};
+const tail = (arr) => arr.slice(1);
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected} `);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
-// ASSERT EQUAL TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 4);
-assertEqual(5, "Hey mom look at me!");
-
-// TAIL TEST CODE
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Lab");
+module.exports = tail
