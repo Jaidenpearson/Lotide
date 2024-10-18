6a97c4abbc429e3fa85dea380ddf8855df987f0f
Jaidenpearson <[jaidensrpearson@gmail.com]>
2024-10-18 13:25:32 -0700

    refactored assertEqArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,12 @@
+const eqArrays = require("./eqArrays")
+
 const assertArraysEqual = function(arr1, arr2) {
-  if(arr1.length !== arr2.length) {
-    console.log("🛑🛑🛑Arrays are different lengths")
-    return
-  }
-  for(let i = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) {
-      console.log(`🛑🛑🛑Index: ${[i]} is not equal between arrays`)
+  if (!eqArrays(arr1, arr2)) {
+      console.log('🛑🛑🛑 Arrays are not equal')
       return
+    } if (eqArrays(arr1, arr2)) {
+      console.log("✔✔✔ arrays are equal")
     }
-  }
-  console.log("✔✔✔Arrays are identical")
 }
 
 
