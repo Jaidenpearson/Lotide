9de2c5e7648722a3202137884b9d391a375df2e0
Jaidenpearson <[jaidensrpearson@gmail.com]>
2024-10-18 14:50:12 -0700

    middle test file created

--- middle.js
+++ middle.js
@@ -12,23 +12,4 @@ let middle = function(arr) {
   return result
 };
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    console.log("🛑🛑🛑 Arrays are different lengths");
-    return;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.log(`🛑🛑🛑 Index: ${[i]} is not equal between arrays`);
-      return;
-    }
-  }
-  console.log("✔✔✔ Arrays are identical");
-};
-
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+module.exports = middle
